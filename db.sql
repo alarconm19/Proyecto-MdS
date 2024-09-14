7f39ca424dce09bb69eebfd101ba8c01105ee5d9
Pibe19 <marcosalarcon213@gmail.com>
2024-09-14 20:27:36 -0300

    update 2

--- db.sql
+++ db.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS `pweb`;
 USE `pweb`;
 
     CREATE TABLE IF NOT EXISTS `users` (
-        'user_id' INT AUTO_INCREMENT PRIMARY KEY,
-        'username' VARCHAR(255) NOT NULL UNIQUE,
-        'password' VARCHAR(255) NOT NULL
+        `user_id` INT AUTO_INCREMENT PRIMARY KEY,
+        `username` VARCHAR(255) NOT NULL UNIQUE,
+        `password` VARCHAR(255) NOT NULL
     );
