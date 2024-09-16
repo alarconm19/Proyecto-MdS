8cd130c6ed1b6fd73d1a43a24b20ef66c1ece5fa
Pibe19 <marcosalarcon213@gmail.com>
2024-09-15 21:45:38 -0300

    update 3

--- db.sql
+++ db.sql
@@ -1,8 +1,22 @@
-CREATE DATABASE IF NOT EXISTS `pweb`;
-USE `pweb`;
+CREATE DATABASE IF NOT EXISTS pweb;
+USE pweb;
 
-    CREATE TABLE IF NOT EXISTS `users` (
-        `user_id` INT AUTO_INCREMENT PRIMARY KEY,
-        `username` VARCHAR(255) NOT NULL UNIQUE,
-        `password` VARCHAR(255) NOT NULL
-    );
+CREATE TABLE IF NOT EXISTS users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    telefono VARCHAR(20),
+    direccion VARCHAR(255),
+    role ENUM('cliente', 'empleado', 'admin') NOT NULL DEFAULT 'cliente'
+);
+
+
+CREATE TABLE IF NOT EXISTS Turnos (
+    turno_id INT AUTO_INCREMENT PRIMARY KEY,
+    cliente_id INT NOT NULL,
+    nombre_servicio VARCHAR(255) NOT NULL,
+    fecha DATE NOT NULL,  -- Solo la fecha
+    hora TIME NOT NULL,   -- Solo la hora
+    FOREIGN KEY (cliente_id) REFERENCES users(user_id)
+);
--- src/routes/login.js
+++ src/routes/login.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const LoginController = require('../controllers/LoginController');
-
 const router = express.Router();
 
 router.get('/login', LoginController.login);
