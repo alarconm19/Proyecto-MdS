876e1fa78760504ea46f345260301502462aaf35
iaraacevedo <iaracevedo3@gmail.com>
2024-09-20 07:00:33 -0300

    rol empleado

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,6 +7,7 @@ const databaseController = require('../controllers/DatabaseController');
 const addSessionData = (req, res, next) => {
     res.locals.loggedin = req.session.loggedin || false;
     res.locals.username = req.session.username || '';
+    res.locals.userRole = req.session.role || ''; 
     next();
 };
 
@@ -88,8 +89,20 @@ router.post('/consultas/crear', databaseController.crearConsulta);
 // Ruta para obtener consultas del cliente
 router.get('/mis-consultas', databaseController.obtenerConsultasCliente);
 
+// Middleware para verificar si el usuario es empleado
+function isEmpleado(req, res, next) {
+    console.log('Middleware isEmpleado ejecutado');
+    console.log('Rol de usuario:', req.session.role);
+    if (req.session.loggedin && req.session.role === 'empleado') {
+        return next();
+    }
+    console.log('Acceso denegado');
+    res.redirect('/'); // Redirige si no es empleado
+}
+
+
 // Ruta para obtener todas las consultas (empleados)
-router.get('/todas', (req, res) => {
+router.get('/todas', isEmpleado, (req, res) => {
     databaseController.obtenerTodasConsultas(req, res, (err, consultas) => {
         if (err) {
             return res.status(500).send('Error al obtener consultas.');
@@ -101,7 +114,7 @@ router.get('/todas', (req, res) => {
 });
 
 // Ruta para responder a una consulta
-router.post('/responder/:id', databaseController.responderConsulta);
+router.post('/responder/:id', isEmpleado, databaseController.responderConsulta);
 
 // Middleware para manejar errores 404 (Página no encontrada)
 router.use((req, res, next) => {
