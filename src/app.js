9bbddcba5e0a28c5416aec68f544a1659fd02948
Pibe19 <marcosalarcon213@gmail.com>
2024-10-07 19:57:17 -0300

    Revert "Merge pull request #1 from alarconm19/test"
    This reverts commit ed3de0572e268b26ea25126b70072f170671ec03, reversing
    changes made to 7645190003c3d98d3915baff18149c909ef80aaf.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 const express = require ('express');
 const { engine } = require ('express-handlebars');
-//const myconection = require ('express-myconnection');
-//const mysql = require ('mysql');
 const session = require ('express-session');
 const path = require('path');
 const mysql2 = require('mysql2');
@@ -17,49 +15,26 @@ const PORT = process.env.PORT || 8080;
 
 app.set('views', __dirname + '/views');
 app.engine('.hbs', engine ({
-    helpers: {
-        eq: (a, b) => a === b },
     extname: '.hbs'
 }));
 app.set('view engine', '.hbs');
 
-
 // Configura la carpeta public para archivos estáticos
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-// app.use(myconection(mysql, {
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASS,
-//     port: 3306,
-//     database: 'pweb'
-// }, 'single'));
-
 var config =
 {
-    host: 'localhost',
-    user: 'root',
-    password: 'iara1611',
+    host: process.env.AZDB_HOST,
+    user: process.env.AZDB_USER,
+    password: process.env.AZDB_PASS,
     database: 'pweb',
     port: 3306,
-    // ssl: {
-    //     rejectUnauthorized: true,
-    //     ca: fs.readFileSync(certPath, "utf8")
-    // }
+    ssl: {
+        rejectUnauthorized: true,
+        ca: fs.readFileSync(certPath, "utf8")
+    }
 };
 
-// const conn = new mysql2.createConnection(config);
-// conn.connect(
-//     function (err) {
-//     if (err) {
-//         console.log("!!! Cannot connect !!! Error:");
-//         throw err;
-//     }
-//     else
-//     {
-//         console.log("Connection established.");
-//     }
-// });
 
 // Middleware para conectar la base de datos
 app.use((req, res, next) => {
@@ -73,28 +48,6 @@ app.use((req, res, next) => {
         }
     });
 });
-
-
-
-// const mysql2 = require('promise-mysql');
-
-// // createUnixSocketPool initializes a Unix socket connection pool for
-// // a Cloud SQL instance of MySQL.
-// const createUnixSocketPool = async config => {
-//   // Note: Saving credentials in environment variables is convenient, but not
-//   // secure - consider a more secure solution such as
-//   // Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
-//   // keep secrets safe.
-//   return mysql2.createPool({
-//     user: process.env.DB_USER, // e.g. 'my-db-user'
-//     password: process.env.DB_PASS, // e.g. 'my-db-password'
-//     database: 'pweb', // e.g. 'my-database'
-//     socketPath: process.env.INSTANCE_UNIX_SOCKET, // e.g. '/cloudsql/project:region:instance'
-//     // Specify additional properties here.
-//     ...config,
-//   });
-// };
-
 
 app.use(session({
     secret: 'your-secret-key',
